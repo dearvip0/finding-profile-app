4dc4258bb05e5d667b441b37619d6064d98bd412
thuongvb <baothuong9608@gmail.com>
2022-01-08 14:43:14 +0700

    first and finally done react finding profile app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
+const Home = () => {
+  return (
+    <div>
+      <UserSearch />
+      <UserResults />
+    </div>
+  );
+};
+
+export default Home;
